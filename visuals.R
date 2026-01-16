4f82cb21c208e920f21dc40a2568aa0064b01ad6
yukizhou24 <lsh2500544@student.lshtm.ac.uk>
2026-01-16 15:59:45 +0000

    Update visuals.R

--- visuals.R
+++ visuals.R
@@ -3,5 +3,3 @@ fev1 <- read_csv(here("2491_eda-main/data/fev1.csv"), col_types = list('id' = 'f
 ggplot(fev1, aes(x = age, y = FEV1)) + geom_point() + geom_smooth(method = 'loess') +
   labs(title = "Relationship between FEV1 and Age", x = "Age (years)", y = "FEV1 (L)") +
   theme_classic()
-
-new()
