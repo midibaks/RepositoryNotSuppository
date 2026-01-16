f78dd7ae43bef245c2403a5a9144fd6161ffda20
Lysette Moses <lysette.moses1@student.lshtm.ac.uk>
2026-01-16 16:30:49 +0000

    Update Suppository1.R

--- Suppository1.R
+++ Suppository1.R
@@ -9,3 +9,18 @@ fev1_sampled <- fev1 %>%
   slice_sample(n = 20) %>%
   select(id) %>%
   inner_join(fev1)
+
+fev1_sampled
+
+class(fev1$id)
+
+fev1_sampled$id <- as.factor(fev1_sampled$id)
+
+ggplot(data = fev1_sampled, aes(x = age, y = FEV1, colour = id)) +
+  geom_point() + 
+  labs(x = "Age (years)",
+       y = "FEV1 score (litres)") +
+  theme(
+    panel.grid = element_blank(),
+    panel.border = element_blank()
+  )
